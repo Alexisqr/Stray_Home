8646c4573d58ea7c128cc8283d9f6dc126d63d72
Alex <alexis.cv1824@gmail.com>
2024-03-21 15:20:41 +0200

    add AddListOfAnimalsCommand and AddListOfAnimalsCommandHandler

---
+++ StrayHome.Application/Features/Animal/Commands/AddListOfAnimals/AddListOfAnimalsCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrayHome.Application.Features.Animal.Commands.AddListOfAnimals
+{
+    internal class AddListOfAnimalsCommandHandler
+    {
+    }
+}
